fb0e852795805facde08e0910c2d583d38fa2638
Gökhan Özcan <gokhanozcan87@gmail.com>
2022-07-07 18:37:53 +0300

    Yorumlar eklendi.

--- son/Form1.cs
+++ son/Form1.cs
@@ -6,6 +6,7 @@ namespace son
 {
     public partial class Form1 : Form
     {
+        //sql baðlantýsý için komutlar
         SqlConnection con;
         SqlCommand cmd;
         SqlDataReader dr;
@@ -19,16 +20,19 @@ namespace son
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // apimin keyi
             string api = "35e94a0fea42d6ba4a7c413a392728e4";
-            
+            //apimin baðlantýsý
             string connection = "http://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=35e94a0fea42d6ba4a7c413a392728e4";
 
             XDocument weather = XDocument.Load(connection);
-            var temp = weather.Descendants("temperature").ElementAt(0).Attribute("value").Value;
-            var pre = weather.Descendants("pressure").ElementAt(0).Attribute("value").Value;
+            //sýcaklýk deðeri, basýnç,güneþ yükseliþi ve þehiri tanýmladým.
+            var temp  =  weather.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var pre   = weather.Descendants("pressure").ElementAt(0).Attribute("value").Value;
             var gunes = weather.Descendants("sun").ElementAt(0).Attribute("rise").Value;
-            var hiz = weather.Descendants("speed").ElementAt(0).Attribute("value").Value;
+            var hiz   = weather.Descendants("speed").ElementAt(0).Attribute("value").Value;
             var sehir = weather.Descendants("city").ElementAt(0).Attribute("name").Value;
+            // Burasý butona týklandýðýnda apiden çektiði veriyi database' e kaydediyor.
             cmd = new SqlCommand();
             con.Open();
             MessageBox.Show("Baþarýyla eklendi.");
@@ -36,6 +40,7 @@ namespace son
             cmd.CommandText = "insert into durum(sýcaklýk,basýnç,gunes,hýz,sehir) values (" + temp + ",'" + pre + "','" + gunes+ "','" + hiz + "','"+ sehir+"')";
             cmd.ExecuteNonQuery();
             con.Close();
+            // bu method veritabanýnýn datagridview da gösterilmesini saðlar.
             giris();
 
 
@@ -45,6 +50,7 @@ namespace son
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //açýlýþ ekranýnda datagridview da database verilerini çaðýrdým.
             giris();    
         }
         void giris()
